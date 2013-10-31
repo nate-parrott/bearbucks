b5b3c3ef585114815cf7a90c747c196af9023f06
Nate Parrott <nateparro2t@gmail.com>
2013-10-31 02:39:58 -0400

    Added hooks for Google analytics

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,15 @@
 var scrape = require('../scrape')
+var fs = require('fs');
+
+var analytics = '';
+if (fs.existsSync('analytics.html')) {
+	analytics = fs.readFileSync('analytics.html');
+}
+
 
 exports.index = function(req, res) {
 	var error = req.query.error? req.query.error : '';
-	res.render('index', {error: error});
+	res.render('index', {error: error, analytics: analytics});
 };
 
 exports.balances = function(req, res) {
@@ -12,6 +19,7 @@ exports.balances = function(req, res) {
 		if (balances.flexPoints != undefined) {
 			var params = balances;
 			params.username = username;
+			params.analytics = analytics;
 			res.render('balances', params);
 		} else {
 			res.redirect('/?error=login')
