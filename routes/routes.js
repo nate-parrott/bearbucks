f223409ec76f2d514b004a954317ca896063b94b
Nate Parrott <nateparro2t@gmail.com>
2013-10-31 15:15:00 -0400

    Added Google Analytics

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,8 @@
 var scrape = require('../scrape')
-var fs = require('fs');
-
-var analytics = '';
-if (fs.existsSync('analytics.html')) {
-	analytics = fs.readFileSync('analytics.html');
-}
-
 
 exports.index = function(req, res) {
 	var error = req.query.error? req.query.error : '';
-	res.render('index', {error: error, analytics: analytics});
+	res.render('index', {error: error});
 };
 
 exports.balances = function(req, res) {
@@ -19,7 +12,6 @@ exports.balances = function(req, res) {
 		if (balances.flexPoints != undefined) {
 			var params = balances;
 			params.username = username;
-			params.analytics = analytics;
 			res.render('balances', params);
 		} else {
 			res.redirect('/?error=login')
